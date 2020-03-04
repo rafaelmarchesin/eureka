5f3944cfaeb6597b01faae2dd209d5bdb69a39a6
Rafael Marchesin <rmarchesin@gmail.com>
2020-03-04 09:23:59 -0300

    feat: Primeiras linhas de código implementadas

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esta é a Eureka!</title>
+</head>
+<body>
+    <?php
+        echo '<h1>Eureka!</h1>';
+        echo '<p>Esta é a home page do sistema Eureka! Estamos só começando! O futuro nos reserva coisas grandiosas, sem dúvida!</p>';
+    ?>
+</body>
+</html>
---
+++ modulos/Lista.php
@@ -0,0 +1,18 @@
+<?php
+
+//require_once './modelos/ModeloLista.php';
+
+use 
+
+abstract class Lista extends ModeloLista
+{
+    //Instancia a classe ModeloLista
+    
+    // Entrada de dados
+
+    // Armazenar dados
+
+    // Leitura de dados
+
+    // Apagar dados
+}
