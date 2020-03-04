574b6f00922e6b8e6170f830e2a68ccd0288a659
Rafael Marchesin <rmarchesin@gmail.com>
2020-03-04 16:24:03 -0300

    feat: Recursos de formulário implementados

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esta é a Eureka!</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./estilo/style.css">
 </head>
 <body>
     <?php
+        //Constrói o cabeçalho da página
         require './view/header.php';
+
+        //Constrói o corpo da página
         require './view/body.php';
+
+        //Constrói o rodapé da página
         require './view/footer.php';
     ?>
 </body>
--- view/body.php
+++ view/body.php
@@ -1,1 +1,9 @@
 <p>AQUI ENTRA A LISTA</p>
+<form action="index.php" method="POST">
+    <input type="text" class="insere-tarefa" id="insere-tarefa" name="insere-tarefa">
+    <input type="submit" value="Gravar">
+</form>
+
+<?php 
+    require 'teste.php';
+    echo "<h2>Seu nome é: {$teste}</h2>";
