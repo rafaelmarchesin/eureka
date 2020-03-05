df73ba4a35a9ea3f82f0884c7ca5b02cdf6ba1cc
Rafael Marchesin <rmarchesin@gmail.com>
2020-03-05 10:11:30 -0300

    feat: Criado frontend da aplicação

--- view/lista-tarefas/body.php
+++ view/lista-tarefas/body.php
@@ -1,3 +1,20 @@
+<div class="container" id="container">
+    <div class="entrada-dados-container" id="entrada-dados-container">
+        <form action="index.php" method="POST">
+            <input type="text" class="entrada-dados" id="entrada-dados" name="tarefa" placeholder="Nova tarefa...">
+        </form>
+    </div>
+    <div class="lista" id="lista">
+        <div class="container-tarefa" id="container-tarefa">
+            <input type="checkbox" class="tarefa" id="tarefa" name="tarefa" value="tarefa">
+            Tarefa 1
+        </div>
+    </div>
+</div>
+
+
+
+
 <p>AQUI ENTRA A LISTA</p>
 <form action="index.php" method="POST">
     <input type="text" class="insere-tarefa" id="insere-tarefa" name="insere-tarefa">
